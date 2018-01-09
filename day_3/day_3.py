4e1a1055f0084fab9739c02ea01a64e4e723ab98
Ola Tobiszewska <aleksp08@gmail.com>
2018-01-09 16:43:17 +0100

    dzień 3 - zadanie 1 i 2

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -18,10 +18,43 @@ def part_1(text):
     return len(houses_with_present)
 
 
+def move(i, position):
+    if i == ">":
+        position[0] += 1
+    elif i == "<":
+        position[0] -= 1
+    elif i == "^":
+        position[1] += 1
+    else:
+        position[1] -= 1
+    return position
+
+
+def part_2(text):
+    santa_position = [0, 0]
+    robo_santa_position = [0, 0]
+    houses_with_present = []
+
+    for i in range(0, len(text)):
+        if i % 2 == 0:
+            santa_position = move(text[i], santa_position)
+        else:
+            robo_santa_position = move(text[i], robo_santa_position)
+
+        if santa_position not in houses_with_present:
+            houses_with_present.append(santa_position.copy())
+
+        if robo_santa_position not in houses_with_present:
+            houses_with_present.append(robo_santa_position.copy())
+
+    return len(houses_with_present)
+
+
 def main():
     with open("input.txt", "r") as file_input:
         input_text = file_input.read()
         print(part_1(input_text))
+        print(part_2(input_text))
 
 
 if __name__ == '__main__':
