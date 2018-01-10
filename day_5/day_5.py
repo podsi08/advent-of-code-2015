fce4aeccac165ad16fe80373ff503f1fb9b69394
Ola Tobiszewska <aleksp08@gmail.com>
2018-01-10 22:42:51 +0100

    dzień 5 - zadanie 1 i 2

--- day_5/day_5.py
+++ day_5/day_5.py
@@ -21,33 +21,24 @@ def part_1(word):
 
 def part_2(word):
     pair_of_letters = False
-    history_of_pairs_1 = []
-    history_of_pairs_2 = []
     repeat_letters = False
 
-    for i in range(0, len(word)):
-        if i <= (len(word) - 2):
-            if word[i] != word[i + 1]:
-                if (word[i] + word[i + 1]) in history_of_pairs_1 or (word[i] + word[i + 1]) in history_of_pairs_2:
-                    pair_of_letters = True
-            else:
-                if i % 2 == 0 and (word[i] + word[i + 1]) in history_of_pairs_1:
-                    pair_of_letters = True
-                elif i % 2 != 0 and (word[i] + word[i + 1]) in history_of_pairs_2:
-                    pair_of_letters = True
+    for i in range(0, len(word) - 1):
+        part_i = word[i:i+2]
+        for j in range(i + 2, len(word) - 1):
+            part_j = word[j:j+2]
+            if part_i == part_j:
+                pair_of_letters = True
+                break
 
-            if i % 2 == 0:
-                history_of_pairs_1.append(word[i] + word[i + 1])
-            else:
-                history_of_pairs_2.append(word[i] + word[i + 1])
-
-        if i <= (len(word) - 3):
+        if i < (len(word) - 2):
             if word[i] == word[i + 2]:
                 repeat_letters = True
 
         if pair_of_letters and repeat_letters:
             return True
-            break
+
+    return False
 
 
 def main():
@@ -61,7 +52,8 @@ def main():
     with open("input.txt", "r") as file_input:
         nice_strings_2 = 0
         for line in file_input:
-            if part_2(line.strip('\n')):
+            word = line.strip()
+            if part_2(word):
                 nice_strings_2 += 1
         print(nice_strings_2)
 
