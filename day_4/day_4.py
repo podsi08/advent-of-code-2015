4963a3a55b09e8c6f40cd14f04e05efbbd4f9b16
Ola Tobiszewska <aleksp08@gmail.com>
2018-01-14 13:31:25 +0100

    dzień 4 - zadanie 1 i 2

--- day_4/day_4.py
+++ day_4/day_4.py
@@ -1,10 +1,30 @@
 from hashlib import md5
 
-def main():
-    with open("input.txt", "r") as file_input:
-        pass
 
-    print(md5("Ola".encode()).digest())
+def part_1(data):
+    result = ""
+    add = 1
+    while result[:5] != "00000":
+        text = data + str(add)
+        result = md5(text.encode()).hexdigest()
+        add += 1
+    return add - 1
+
+
+def part_2(data):
+    result = ""
+    add = 1
+    while result[:6] != "000000":
+        text = data + str(add)
+        result = md5(text.encode()).hexdigest()
+        add += 1
+    return add - 1
+
+
+def main():
+    data = "yzbqklnj"
+    print(part_1(data))
+    print(part_2(data))
 
 
 if __name__ == '__main__':
