64bdbb9c232d589c692abb4599434ba19ba81405
Ola Tobiszewska <aleksp08@gmail.com>
2018-01-30 23:36:13 +0100

    dzień 12 - zadanie 1 i 2

--- day_12/day_12.py
+++ day_12/day_12.py
@@ -1,3 +1,6 @@
+import json
+
+
 def part_1(text):
     numbers_m = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "-"]
     total = 0
@@ -11,44 +14,48 @@ def part_1(text):
     return total
 
 
-def part_2(text):
-    numbers_m = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "-"]
+def sum_array(array):
+    sum_a = 0
+    for element in array:
+        if type(element) is int:
+            sum_a += element
+        elif type(element) is list:
+            sum_a += sum_array(element)
+        elif type(element) is dict:
+            sum_a += sum_object(element)
+    return sum_a
+
+
+def sum_object(object):
+    sum_o = 0
+    if "red" not in object.values():
+        for value in object:
+            if type(object[value]) is int:
+                sum_o += object[value]
+            elif type(object[value]) is list:
+                sum_o += sum_array(object[value])
+            elif type(object[value]) is dict:
+                sum_o += sum_object(object[value])
+    return sum_o
+
+
+def part_2(lst):
     total = 0
-    to_add = ""
-    sum_in_object = 0
-    open_object = False
-    red_object = False
-    for i in range(len(text) - 1):
-        if not open_object:
-            if text[i] in numbers_m:
-                to_add += text[i]
-                if text[i + 1] not in numbers_m:
-                    total += int(to_add)
-                    to_add = ""
-            elif text[i] == "{":
-                open_object = True
-        else:
-            if text[i:i+3] == "red":
-                red_object = True
-            if text[i] == "}":
-                open_object = False
-                red_object = False
-                if not red_object:
-                    total += sum_in_object
-                    sum_in_object = 0
-            if not red_object and text[i] in numbers_m:
-                to_add += text[i]
-                if text[i + 1] not in numbers_m:
-                    sum_in_object += int(to_add)
-                    to_add = ""
+    for i in lst:
+        if type(i) is list:
+            total += sum_array(i)
+        elif type(i) is dict:
+            total += sum_object(i)
+        elif type(i) is int:
+            total += i
     return total
 
-# uwzględnić obiekty w obiektach
+
 def main():
     with open("input.txt", "r") as file_input:
         text = file_input.read()
         print(part_1(text))
-        print(part_2(text))
+        print(part_2(json.loads(text)))
 
 
 if __name__ == '__main__':
