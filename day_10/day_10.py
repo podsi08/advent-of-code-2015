437c1377e1ffc93e51b61789b4d772565c75051d
Ola Tobiszewska <aleksp08@gmail.com>
2018-01-30 09:18:33 +0100

    dzień 10 - zadanie 1 i 2

--- day_10/day_10.py
+++ day_10/day_10.py
@@ -1,29 +1,29 @@
-def look_and_say(text):
+def look_and_say(text, n):
     step = 0
     count = 0
     new_text = ""
-    history = []
-    while step <= 40:
-        for i in range(0, len(text)):
-            if i < (len(text) - 1):
-                if text[i] == text[i + 1]:
-                    count += 1
-                else:
-                    if count > 0:
-                        count += 1
-                        new_text += str(count) + text[i]
-                        count = 0
-                    else:
-                        new_text += "1" + text[i]
-            else:
 
-        history.append(new_text)
+    while step < n:
+        for i in range(0, len(text)):
+            if i == (len(text) - 1) or text[i] != text[i + 1]:
+                if count > 0:
+                    count += 1
+                    new_text += str(count) + text[i]
+                    count = 0
+                else:
+                    new_text += "1" + text[i]
+            elif text[i] == text[i + 1]:
+                count += 1
+        text = new_text
+        new_text = ""
         step += 1
+    return len(text)
 
 
 def main():
     puzzle_input = "1113222113"
-    print(look_and_say(puzzle_input))
+    print(look_and_say(puzzle_input, 40))
+    print(look_and_say(puzzle_input, 50))
 
 
 if __name__ == '__main__':
